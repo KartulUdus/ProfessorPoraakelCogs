6c1af81b39cc3902a4b4d94849a3297ac11c2733
Frost The Fox <Frost@frostthefox.pw>
2017-03-15 15:50:09 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# RocketMap-cogs
---
+++ SpriteCog/SpriteCog.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+
+
+class sprite:
+    """Send sprite information"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True)
+    async def sprite(self, ctx):
+        message = """*Psst. You can grab the sprites at https://stuff.notfurprofit.org/static03.zip*
+
+Simply extract the "static" folder into the root of RocketMap. Replace any files in conflict.
+
+Mac OS Users: You must manually extract the icons folder and the png files into the static folder.
+
+If you would like sprites that go up to gen4, you may use https://stuff.notfurprofit.org/static02.zip.
+
+SpriteBot v2.3
+        """
+        await self.bot.whisper(message)
+        if not ctx.message.channel.is_private:
+            await self.bot.delete_message(ctx.message)
+
+
+def setup(bot):
+    bot.add_cog(sprite(bot))
