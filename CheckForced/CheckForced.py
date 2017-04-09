06462b190a01a7bd204ce713b20c9e4bcfd25f4e
FrostTheFox <thunderfox@thunderfox.nl>
2017-04-08 22:05:54 -0400

    First test of CheckForced

---
+++ CheckForced/CheckForced.py
@@ -0,0 +1,31 @@
+import discord
+import requests
+from discord.ext import commands
+
+
+class checkforced:
+    """Cog for API force checking."""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def checkforced(self):
+        """Grabs latest forced API version."""
+
+        pr = {
+                 http: ''
+        }
+
+        try:
+            r = requests.get('https://pgorelease.nianticlabs.com/plfe/version', proxies=pr)
+            if r.status_code == 200:
+                await self.bot.say(r.content)
+            if r.status_code == 403:
+                await self.bot.say(':x: I was refused service by Niantic. My proxy must be banned :(')
+        except requests.exceptions.RequestException as e:
+            await self.bot.say(':x: I was unable to connect to Niantic.')
+
+
+def setup(bot):
+    bot.add_cog(checkforced(bot))
---
+++ CheckForced/README.md
@@ -0,0 +1,8 @@
+This cog allows to check against /plfe/version to determine the current API version Niantic requires to connect to PoGo.
+
+Setup Instructions:
+* Install the cog using Red's downloader, or by downloading the .py and placing it in your Red's cogs folder.
+* Disable the CheckForced cog if it was automatically enabled.
+* Open CheckForced.py in Red's cogs folder, and at line 17, between the '' of http: '', place the IP address and port of your proxy. Ex: `http: '101.8.101.91:3128'`
+* Save the .py file, and enable the CheckForced cog.
+* Test with !checkforced
