2b8869bb508f4fb67867385f3058372bde664ca5
FrostTheFox <thunderfox@thunderfox.nl>
2017-04-13 16:15:47 -0400

    Add 5s timeout to checkproxy (in an effort to prevent bot hanging

--- CheckProxy/CheckProxy.py
+++ CheckProxy/CheckProxy.py
@@ -19,11 +19,13 @@ class checkproxy:
                     'https': p
                 }
         try:
-            r = requests.get('https://pgorelease.nianticlabs.com/plfe/version', proxies=pr)
+            r = requests.get('https://pgorelease.nianticlabs.com/plfe/version', proxies=pr, timeout=5)
             if r.status_code == 200:
                 await self.bot.say(':white_check_mark: 200 OK, proxy is not banned.')
             if r.status_code == 403:
                 await self.bot.say(':x: 403 Forbidden, proxy is banned.')
+        except requests.exceptions.timeout:
+            await self.bot.say(':x: Timed out checking proxy.')
         except requests.exceptions.RequestException as e:
             await self.bot.say('Something is wrong with your proxy. Make sure to put the port as well as remove http or https from your input. Authentication is not supported right now.')
 
