f094a6719590306e6e151675dfd12c4b858218ef
FrostTheFox <thunderfox@thunderfox.nl>
2017-05-16 18:59:51 -0400

    Add cries to spritecog

--- SpriteCog/SpriteCog.py
+++ SpriteCog/SpriteCog.py
@@ -23,6 +23,20 @@ SpriteBot v2.3
         await self.bot.whisper(message)
         if not ctx.message.channel.is_private:
             await self.bot.delete_message(ctx.message)
+    @commands.command(pass_context=True)
+    async def cries(self, ctx):
+        message = """*Psst. You can grab cries at https://stuff.notfurprofit.org/cries01.zip*
+
+Simply extract the "static" folder into the root of RocketMap. Replace any files in conflict.
+
+Mac OS Users: You must manually extract the cries folder from static/sounds into the static/sounds folder of RocketMap.
+
+SpriteBot v2.3
+        """
+        await self.bot.whisper(message)
+        if not ctx.message.channel.is_private:
+            await self.bot.delete_message(ctx.message)
+
 
 
 def setup(bot):
