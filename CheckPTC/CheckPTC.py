7f252ec7f9dbac6690d93929b629f1afe70c51a6
FrostTheFox <thunderfox@thunderfox.nl>
2017-05-20 23:35:16 -0400

    Rewrite CheckPTC

--- CheckPTC/CheckPTC.py
+++ CheckPTC/CheckPTC.py
@@ -1,124 +1,84 @@
-# taken from pgoapi's auth_ptc.
-
-from __future__ import absolute_import
-from future.standard_library import install_aliases
-install_aliases()
-
-import re
-import six
-import json
-import logging
-import requests
-import discord
-import random
-
+from requests_futures.sessions import FuturesSession
 from urllib.parse import parse_qs
-from discord.ext import commands
-
-from requests.exceptions import ConnectionError
+import logging
+import json
 
 class checkptc():
-    """checks if ptc is working using pgoapi's login code"""
+    """checks if PTC is working properly."""
 
-    PTC_LOGIN_URL = 'https://sso.pokemon.com/sso/login?service=https%3A%2F%2Fsso.pokemon.com%2Fsso%2Foauth2.0%2FcallbackAuthorize'
-    PTC_LOGIN_OAUTH = 'https://sso.pokemon.com/sso/oauth2.0/accessToken'
-    PTC_LOGIN_CLIENT_SECRET = 'w8ScCUXJQc6kXKw8FiOhd8Fixzht18Dq3PEVkUCP5ZPxtgyWsbTvWHFLm2wNY0JR'
+    loginurl = 'https://sso.pokemon.com/sso/login?service=https%3A%2F%2Fsso.pokemon.com%2Fsso%2Foauth2.0%2FcallbackAuthorize'
+    oauth = 'https://sso.pokemon.com/sso/oauth2.0/accessToken'
+    clientsecret = 'w8ScCUXJQc6kXKw8FiOhd8Fixzht18Dq3PEVkUCP5ZPxtgyWsbTvWHFLm2wNY0JR'
 
     def __init__(self, bot):
 
         self.bot = bot
+        with open('CheckPTC.json', 'r') as f:
+            config = json.load(f)
 
-        self._auth_provider = 'ptc'
-
-        self._session = requests.session()
-        self._session.verify = True
-
-    def set_proxy(self, proxy_config):
-        self._session.proxies = proxy_config
     @commands.command()
     async def checkptc(self):
         """Checks the current status of PTC login."""
-        logging.info('Trying to login to PTC with tfoxmap5')
+        logging.info('Trying to login to PTC.')
 
         head = {'User-Agent': 'niantic'}
+        session = FuturesSession()
 
         try:
-            r = self._session.get(self.PTC_LOGIN_URL, headers=head, timeout=15)
-        except ConnectionError as e:
-            await self.bot.say(':x: Timed out or failed to connect completely. PTC is probably having issues.', e)
-
+            r1 = session.get(loginurl, headers=head, timeout=15)
+        except BaseException as e:
+            await self.bot.say(':x: Timed out or failed to connect. PTC is probably having issues.')
+            logging.info('Failed to connect to PTC: %s', e)
+            return False
         try:
-            jdata = json.loads(r.content.decode('utf-8'))
+            r1data = json.loads(r1.result().content.decode('utf-8'))
             data = {
-                'lt': jdata['lt'],
-                'execution': jdata['execution'],
+                'lt': r1data['lt'],
+                'execution': r1data['execution'],
                 '_eventId': 'submit',
-                'username': 'USER',
-                'password': 'PASSWORD',
+                'username': config['username'],
+                'password': config['password'],
             }
-        except ValueError as e:
-            logging.error('PTC User Login Error - Field missing in response: %s', e)
-            await self.bot.say(':x: Alright something is probably wrong on my end. Missing field in PTC response. If PTC is having problems, though, this could happen.')
-            self._session.get('https://club.pokemon.com/us/pokemon-trainer-club/logout')
+        except BaseException as e:
+            logging.error('Failed to GET lt and execution')
+            await self.bot.say(':x: Failed initial auth GET, PTC might be having issues.')
+            session.get('https://club.pokemon.com/us/pokemon-trainer-club/logout')
             return False
-        except KeyError as e:
-            logging.error('PTC User Login Error - Field missing in response.content: %s', e)
-            await self.bot.say(':x: Alright something is probably wrong on my end. Missing field in PTC response. If PTC is having problems, though, this could happen.')
-            self._session.get('https://club.pokemon.com/us/pokemon-trainer-club/logout')
-            return False
-
-        r1 = self._session.post(self.PTC_LOGIN_URL, data=data, headers=head, timeout=10)
-
-        ticket = None
         try:
-            ticket = re.sub('.*ticket=', '', r1.history[0].headers['Location'])
-        except Exception as e:
-            try:
-                await self.bot.say(':x: PTC is probably down. Failed to get a token.')
-                self._session.get('https://club.pokemon.com/us/pokemon-trainer-club/logout')
-            except Exception as e:
-                await self.bot.say(':x: PTC is probably down. Failed to get a token.')
-                self._session.get('https://club.pokemon.com/us/pokemon-trainer-club/logout')
+            r2 = session.post(loginurl, data=data, headers=head, timeout=15)
+        except BaseException as e:
+            logging.error('Failed to POST login data.')
+            await self.bot.say(':x: Failed to login to PTC.')
             return False
-
-        self._refresh_token = ticket
-        logging.info('PTC User Login successful.')
-
-        logging.info('Trying to grab an access token.')
-        data1 = {
-            'client_id': 'mobile-app_pokemon-go',
-            'redirect_uri': 'https://www.nianticlabs.com/pokemongo/error',
-            'client_secret': self.PTC_LOGIN_CLIENT_SECRET,
-            'grant_type': 'refresh_token',
-            'code': self._refresh_token,
-        }
-
-        r2 = self._session.post(self.PTC_LOGIN_OAUTH, data=data1, timeout=10)
-
-        qs = r2.content.decode('utf-8')
-        token_data = parse_qs(qs)
-
-        access_token = token_data.get('access_token', None)
-        if access_token is not None:
-            self._access_token = access_token[0]
-
-            self._login = True
-
-            logging.info('PTC Access Token successfully retrieved.')
-            await self.bot.say(':white_check_mark: Logged into PTC successfully. \n' +
-            'Token: `' + self._access_token[:25] + '`')
-            logging.debug('PTC Access Token: %s...', self._access_token[:25])
-            self._session.get('https://club.pokemon.com/us/pokemon-trainer-club/logout')
-        else:
-            self._access_token = None
-            self._login = False
-            await self.bot.say(':x: Logged in, but unable to get an access token. PTC is probably having issues.')
-            logging.info("Could not retrieve a PTC Access Token")
-            self._session.get('https://club.pokemon.com/us/pokemon-trainer-club/logout')
-
-    def set_refresh_token(self, refresh_token):
-        logging.info('PTC Refresh Token provided by user')
-        self._refresh_token = refresh_token
+        try:
+            ticket = re.sub('.*ticket=', '', r2.result().history[0].headers['Location'])
+            logging.info('PTC login successful.')
+        except BaseException as e:
+            logging.error('Failed to find ticket')
+            await self.bot.say(':x: Couldn\'t find a valid ticket, PTC is probably having issues.')
+            return False
+        try:
+            logging.info('Grabbing PoGo access token')
+            accessdata = {
+                'client_id': 'mobile-app_pokemon-go',
+                'redirect_uri': 'https://www.nianticlabs.com/pokemongo/error',
+                'client_secret': clientsecret,
+                'grant_type': 'refresh_token',
+                'code': ticket
+            }
+            r3 = session.post(oauth, data=accessdata, timeout=15)
+            qs = parse_qs(r3.result().content.decode('utf-8'))
+            accesstoken = qs.get('access_token', None)
+            if accesstoken is not None:
+                logging.info('PTC access token successfully retrieved.' + accesstoken[:25])
+                await self.bot.say(':white_check_mark: Logged into PTC successfully. \n' +
+                                   'Token: `' + accesstoken[:25] + '`')
+                session.get('https://club.pokemon.com/us/pokemon-trainer-club/logout')
+            else:
+                logging.info('Logged in, but failed to get access token.')
+                await self.bot.say(':x: Logged in, but unable to get an access token. PTC is probably having issues.')
+                session.get('https://club.pokemon.com/us/pokemon-trainer-club/logout')
+                return False
 
 
 def setup(bot):
--- CheckPTC/README.md
+++ CheckPTC/README.md
@@ -1,10 +1,7 @@
-This cog is a modified version of the CheckPTC used in the RocketMap server. This version only supports one account, and does not include any account rotation or rate limiting functionality.
+This cog is a modified version of the CheckPTC used in the RocketMap server. This version only supports one account, and does not include any account rotation.
 
 Instructions for using CheckPTC:
 * Install the cog using Red's downloader, or by downloading the .py and placing it in your Red's cogs folder.
-* Disable the CheckPTC cog if it was automatically enabled.
-* Open CheckPTC.py in Red's cogs folder, and edit line 56-57, replace USER with a PTC Username (should not be a main account), and replace PASSWORD with the password of said account.
-* Enable the CheckPTC cog.
-
-PTC Login code belongs to pgoapi.
-py3 conversion/bot compatibility belongs to FrostTheFox.
+* Open the CheckPTC.json in the cogs folder.
+* Enter your PTC username and password.
+* Reload the CheckPTC cog.
