8ee4b109191710c1df051ee768367093517d5963
FrostTheFox <thunderfox@thunderfox.nl>
2017-07-23 14:08:33 -0400

    sprite05

--- SpriteCog/SpriteCog.py
+++ SpriteCog/SpriteCog.py
@@ -10,11 +10,13 @@ class sprite:
 
     @commands.command(pass_context=True)
     async def sprite(self, ctx):
-        message = """*Psst. You can grab the sprites at https://stuff.notfurprofit.org/static04.zip*
+        message = """*Psst. You can grab the sprites at https://stuff.notfurprofit.org/static05.zip*
 
 Simply extract the "static" folder into the root of RocketMap. Replace any files in conflict.
 
 Mac OS Users: You must manually extract the icons folder and the png files into the static folder.
+
+If you don't like the look of sprite05, you may use https://stuff.notfurprofit.org/static04.zip.
 
 SpriteBot v2.4
         """
