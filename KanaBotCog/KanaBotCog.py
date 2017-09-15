c74c1c0314e5f51103665ab933ee61c5d0bf6d66
Derek Mäekask <derek.maekask@myjar.com>
2017-09-15 22:27:27 +0300

    testing4

--- KanaBotCog/KanaBotCog.py
+++ KanaBotCog/KanaBotCog.py
@@ -107,7 +107,8 @@ class kanaBot:
 
     @commands.command(pass_context=True)
     async def kana(self, ctx):
-        kana = discord.server.roles.get("name", "kananägu")
+
+
         if ('&358207117951369217') in discord.Member.roles:
 
             await self.bot.say("lol pleb, you have " + ctx + "to be :chicken: to do that")
