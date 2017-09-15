2cfe2855a58c6335292543b048725e8d4d6a938a
Derek Mäekask <derek.maekask@myjar.com>
2017-09-15 19:50:44 +0300

    initial commit of KanaBotCog

---
+++ KanaBotCog/kanaBot.py
@@ -0,0 +1,29 @@
+import discord
+import paramiko
+from discord.ext import commands
+
+
+class kanaNagu:
+    """Allow kanaNagu to have basic control"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True)
+    async def kana(self, ctx):
+
+        #Something goes here
+
+        if discord.Role.name == 'kananägu':
+
+            await self.bot.say("lol pleb, " + user.mention + " you have to be :chicken: to do that")
+            await self.bot.delete_message(ctx.message)
+        else:
+            print:"potato"
+
+
+
+
+
+def setup(bot):
+    bot.add_cog(kanaNagu(bot))
