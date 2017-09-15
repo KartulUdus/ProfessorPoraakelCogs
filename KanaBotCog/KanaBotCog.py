8ad9e4b91acf5b130421f50e06dee14f0bca6ca9
Derek Mäekask <derek.maekask@myjar.com>
2017-09-15 23:10:03 +0300

    testing4

--- KanaBotCog/KanaBotCog.py
+++ KanaBotCog/KanaBotCog.py
@@ -109,7 +109,7 @@ class kanaBot:
     @commands.command(pass_context=True)
     async def kana(self):
 
-        await self.bot.say("lol pleb, you have " + str(discord.role) + "to be :chicken: to do that")
+        await self.bot.say("lol pleb, you have " + str(discord.role.name) + "to be :chicken: to do that")
 
 
 
