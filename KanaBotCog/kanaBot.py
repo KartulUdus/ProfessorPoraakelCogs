ee03afba5551e18dbfef3fc9046086e51767f5f3
Derek Mäekask <derek.maekask@myjar.com>
2017-09-15 20:01:33 +0300

    some Message

--- KanaBotCog/kanaBot.py
+++ KanaBotCog/kanaBot.py
@@ -12,18 +12,11 @@ class kanaNagu:
     @commands.command(pass_context=True)
     async def kana(self, ctx):
 
-        #Something goes here
 
         if discord.Role.name == 'kananägu':
 
             await self.bot.say("lol pleb, " + user.mention + " you have to be :chicken: to do that")
             await self.bot.delete_message(ctx.message)
-        else:
-            print:"potato"
-
-
-
-
 
 def setup(bot):
     bot.add_cog(kanaNagu(bot))
