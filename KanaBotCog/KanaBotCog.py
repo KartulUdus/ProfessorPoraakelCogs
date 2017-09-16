87f3c4928aa270e5d8dd452023a14256a8201703
Derek Mäekask <derek.maekask@myjar.com>
2017-09-16 03:14:08 +0300

    message fixes test

--- KanaBotCog/KanaBotCog.py
+++ KanaBotCog/KanaBotCog.py
@@ -114,7 +114,7 @@ class kanaBot:
     @kana.command(pass_context=True)
     async def tartu(self):
         sshgo("tartu")
-        await self.bot.say(":white_check_mark: "+ discord.message.author.mention +" tartu restarted")
+        await self.bot.say(":white_check_mark: "+ message.author.mention +" tartu restarted")
 
     @kana.command(pass_context=True)
     async def peetri(self):
