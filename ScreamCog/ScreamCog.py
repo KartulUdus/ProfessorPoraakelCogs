bf605259dcb671748edf9f7129e274405e22fcd2
Derek Mäekask <derek.maekask@myjar.com>
2017-09-16 04:49:47 +0300

    beginning of screamcog

---
+++ ScreamCog/ScreamCog.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+
+
+class telegram:
+    """Send filter information"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True)
+
+    async def scream(self,ctx):
+        msg = ctx.message
+        await self.bot.say(msg)
+
+
+
+def setup(bot):
+    bot.add_cog(telegram(bot))
