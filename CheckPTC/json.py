990797eb3086102bae9909697bde2f09b5ebb6d5
kartuludus <derek.maekask@gmail.com>
2018-12-20 15:10:29 +0200

    testing new cog

---
+++ CheckPTC/json.py
@@ -0,0 +1,35 @@
+from urllib.parse import parse_qs, urlsplit
+from discord.ext import commands
+import logging
+import json
+import discord
+
+try:
+    from requests_futures.sessions import FuturesSession
+    wehaveitall = True
+except:
+    wehaveitall = False
+
+class json:
+    """Gives JSON formatted """
+
+    def __init__(self, bot):
+
+        self.bot = bot
+
+    @commands.command()
+    async def json(self, text*):
+        """Checks the current status of PTC login."""
+        logging.info('Trying parse JSON.')
+
+        try:
+            await self.bot.say(text*)
+        except BaseException as e:
+            logging.error('Failed to GET lt and execution: %s', e)
+            await self.bot.say(':x: something')
+            return False
+
+
+
+def setup(bot):
+    bot.add_cog(json(bot))
