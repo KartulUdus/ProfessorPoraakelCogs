c78ecfa1e0dc5d9cffffc7f9c6dc5a27ce5ec9cc
kartuludus <derek.maekask@gmail.com>
2018-12-20 15:42:51 +0200

    testing new cog

--- KanaBotCog/KanaBotCog.py
+++ KanaBotCog/KanaBotCog.py
@@ -1,161 +1,34 @@
-import discord
-import paramiko
 from discord.ext import commands
-import configargparse
-import os
-import sys
-client = discord.Client()
+import logging
+import json
+import discord
 
-def get_args():
-        # Get full dir and default config file path
-        configfile = []
-        if '-cf' not in sys.argv and '--config' not in sys.argv:
-            configfile = [os.getenv('CONFIG', os.path.join(
-                os.path.dirname(__file__), 'config.ini'))]
-        parser = configargparse.ArgParser(
-            default_config_files=configfile)
+try:
+    from requests_futures.sessions import FuturesSession
+    wehaveitall = True
+except:
+    wehaveitall = False
 
-        # arrrgs, also available in config/config.ini
-
-        parser.add_argument(
-            '-cf',
-            '--config', is_config_file=True,
-            help='path to config file (config.ini by default)')
-
-        parser.add_argument(
-            '-sh',
-            '--scanhost',
-            help='host for scanserver'
-        )
-
-        parser.add_argument(
-            '-su',
-            '--scanuser',
-            help='username for scanserver'
-        )
-
-        parser.add_argument(
-            '-sp',
-            '--scanpassword',
-            help='password for scanserver'
-        )
-
-        parser.add_argument(
-            '-fh',
-            '--fronthost',
-            help='host for frontend server'
-        )
-
-        parser.add_argument(
-            '-fu',
-            '--frontuser',
-            help='host for frontend server'
-        )
-
-        parser.add_argument(
-            '-fp',
-            '--frontpassword',
-            help='password for frontend server'
-        )
-        return parser.parse_args()
-
-def sshgo(cmd):
-        cmds = {
-            'alarm': 'service alarms restart',
-            'tallinn': 'service tallinnmap2 restart',
-            'tartu': 'service tartumap restart',
-            'peetri': 'service peetri restart',
-            'haapsalu': 'service haapsalumap restart',
-            'rakvere': 'service rakveremap restart',
-            'kuressaare': 'service saaremaamap restart',
-            'webserver': 'sh restartAll.sh',
-            'nginx': 'sudo service nginx restart',
-            'dontdothis': 'reboot',
-            'test': 'mkdir thisshouldwork'
-        }
-
-        args = get_args()
-        theCmd = cmds[cmd]
-        be = True
-        if cmd in ("nginx", "webserver"):
-            be = False
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        if (be):
-            ssh.connect(args.scanhost, username=args.scanuser,
-                        password=args.scanpassword)
-            ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(theCmd)
-
-        else:
-            ssh.connect(args.fronthost, username=args.frontuser,
-                        password=args.frontpassword)
-            ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(theCmd)
-
-
-class kanaBot:
-    """Allow kanaNagu to have basic control"""
+class json:
+    """Gives JSON formatted """
 
     def __init__(self, bot):
+
         self.bot = bot
 
+    @commands.command()
+    async def json(self, text*):
+        """Checks the current status of PTC login."""
+        logging.info('Trying parse JSON.')
 
-    @commands.group(pass_context=True)
-    @commands.has_role("kananägu")
-    async def kana(self, ctx):
-        if ctx.invoked_subcommand is None:
-            await self.bot.say('Invalid :chicken:')
+        try:
+            await self.bot.say(text*)
+        except BaseException as e:
+            logging.error('Failed to GET lt and execution: %s', e)
+            await self.bot.say(':x: something')
+            return False
 
-    @kana.command(pass_context=True)
-    async def tallinn(self, ctx):
-       sshgo("tallinn")
-       await self.bot.say(":white_check_mark: "+ (ctx.message.author).mention +" tallinn restarted")
-
-    @kana.command(pass_context=True)
-    async def tartu(self, ctx):
-        sshgo("tartu")
-        await self.bot.say(":white_check_mark: "+ (ctx.message.author).mention +" tartu restarted")
-
-    @kana.command(pass_context=True)
-    async def peetri(self, ctx):
-        sshgo("peetri")
-        await self.bot.say(":white_check_mark: "+ (ctx.message.author).mention +" peetri restarted")
-
-    @kana.command(pass_context=True)
-    async def rakvere(self, ctx):
-        sshgo('rakvere')
-        await self.bot.say(":white_check_mark: "+ (ctx.message.author).mention +" rakvere restarted")
-
-    @kana.command(pass_context=True)
-    async def haapsalu(self, ctx):
-        sshgo("haapsalu")
-        await self.bot.say(":white_check_mark: "+ (ctx.message.author).mention +" haapsalu restarted")
-
-    @kana.command(pass_context=True)
-    async def kuressaare(self, ctx):
-        sshgo("kuressaare")
-        await self.bot.say(":white_check_mark: "+ (ctx.message.author).mention +" kuressaare restarted")
-
-    @kana.command(pass_context=True)
-    async def webserver(self, ctx):
-
-        sshgo("webserver")
-        await self.bot.say(":white_check_mark: "+ (ctx.message.author).mention +" frontend restarted")
-
-    @kana.command(pass_context=True)
-    async def nginx(self, ctx):
-        sshgo("nginx")
-        await self.bot.say(":white_check_mark: "+ (ctx.message.author).mention +" nginx restarted")
-
-    @kana.command(pass_context=True)
-    async def dontdothis(self, ctx):
-        sshgo("dontdothis")
-        await self.bot.say(":white_check_mark: "+ (ctx.message.author).mention +" :hamster: server restarted")
-
-    @kana.command(pass_context=True)
-    async def alarm(self, ctx):
-        sshgo("alarm")
-        await self.bot.say(":white_check_mark: "+ (ctx.message.author).mention +" alarms restarted")
 
 
 def setup(bot):
-    bot.add_cog(kanaBot(bot))
+    bot.add_cog(json(bot))
